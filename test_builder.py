789dae9e76f6d0be14e3f1e80d42eb1e4b2072d4
Kristofer M White <me@kmwhite.net>
2012-06-17 21:22:50 -0500

    Adding basic test framework file

---
+++ test_builder.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+# Standard Library Imports
+import unittest
+
+# Third Party Imports
+
+# Local Imports
+import builder
+
+class TestBuilder(unittest.TestCase):
+
+    def setUp(self):
+        ''' Set up the tests '''
+        pass
+
+    def tearDown(self):
+        ''' Tear Down the tests '''
+        pass
+
+    def test_builder(self):
+        ''' Test '''
