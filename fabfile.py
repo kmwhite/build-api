c5440381129158692ec94531fbf1e04a7761d929
Kristofer M White <me@kmwhite.net>
2012-06-17 21:57:37 -0500

    Adding fabfile

---
+++ fabfile.py
@@ -0,0 +1,44 @@
+from fabric.api import env, local
+import os
+
+env.hosts = ['localhost']
+#env.user = 'deploy'
+#env.keyfile = ['$HOME/.ssh/deploy_rsa']
+#env.directory = '/path/to/virtualenvs/project'
+#env.activate = 'source /path/to/virtualenvs/project/bin/activate'
+
+def test():
+    ''' Run the unit tests '''
+    local('py.test builder test_builder.py')
+
+def coverage():
+    ''' Run the unit tests and generate a coverage report '''
+    local('py.test --cov builder test_builder.py')
+
+def coverage_html():
+    ''' Generate the HTML coverage report '''
+    local('coverage run test_builder.py; coverage html; firefox htmlcov/index.html')
+
+def coverage_loop():
+    ''' Run the coverage report in an infinite loop '''
+    while True:
+        coverage()
+        local('read')
+
+def pep8():
+    ''' Check the code base for PEP8 Compatibility '''
+    local('pep8 builder/')
+
+def pep8_loop():
+    ''' Run the pep8 report in an infinite loop '''
+    while True:
+        pep8()
+        local('read')
+
+def clean():
+    '''
+Clean the *.pyc files from the working tree. Additionally, remove all
+the __pycache__ directories
+'''
+    local('find . -type d -name "*__pycache__*" -exec rm -rv {} \;')
+    local('find . -iname "*.pyc" -exec rm -v {} \;')
